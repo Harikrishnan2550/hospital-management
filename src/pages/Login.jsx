01d1d65728fb8e2c5eec6b2f851f92181a3c5c47
harikrishnan <harik2021a@gmail.com>
2025-03-06 16:02:37 +0530

    chamged

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Login() {
+
+  const [email,setEmail] =  useState('')
+  const [password,setPassword] = useState('')
+  const [name,setName] = useState('')
+
+
   return (
     <div>
       
